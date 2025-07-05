0daa49fa0f637b402f9fc598ef872067397c78a8
akashyadav1330 <yadavakash1330@gmail.com>
2025-07-05 10:33:13 +0530

    data Science

---
+++ Python/basic.py
@@ -0,0 +1,245 @@
+# # print("hello world")
+# # a=input("enter your number:")
+# # print(a)
+# # str = "prashansa"
+# # print(len(str))
+# # print(str[0])
+
+# # a = "prashansa"
+# # b = "SINGH"
+# # print("type of string:", type(a))
+# # print("uppercase:", a.upper())
+# # print("lowercase:", b.lower())
+
+# # print("Ttile:", a.title())
+# # print("capitalize:", b.capitalize())
+# # print("Swapcase:", a.swapcase())
+# # print("a:", a.count('a'))
+
+# # first = "prashansa"
+# # last = "SINGH"
+# # # print(f"My first name is {first} and my last name is {last}")
+# # print(first+" "+last)
+
+# #  LIST
+
+# # a =[2,3,4,5,5,5,"nav"]
+# # print("length of list:",len(a))
+
+# # # indexing
+
+# # print(a[5])
+
+# # slicing\
+# # print(a[0:4])
+
+# # a.append(65)
+# # print(a)
+
+# # a.pop()
+# # print(a)
+
+# # a.pop(2)
+# # print(a)
+
+# # a.insert(3,00)
+# # print(a)
+
+# # a.remove(2)
+# # print(a)
+
+# # a.reverse()
+# # print(a)
+
+# # print(a.count(5))
+
+# # b =[1,3,5,2,7,5,8,3,4,75,36,84]
+# # b.sort()
+# # print(b)
+
+# # tuple
+
+# # tpl=(5,8,2,6,"prash")
+# # print("Type of tpl:", type(tpl))
+# # print("length of tuple:", len(tpl))
+
+# # # indexing
+# # print(tpl[3])
+
+# # # slicing
+
+# # print(tpl[:-1])
+# # print(tpl[-1:])
+
+# # abc =1,2,3
+# # a,b,c =abc
+# # print(a)
+# # print(b)
+# # print(c)
+
+# # t1= (1,23,4,5,6,7,4)
+
+# # t2= (1,23,4,5,6,7,4)
+# # print(t1+t2)
+# # print(t1*4)
+
+# # tl = (1,2,3,4,5,8,8,[1,2,7,8])
+
+# # a=(tl[7])
+# # print(a[2:4])
+# # print(tl[7][2:4])
+
+# # # dictionary
+
+# # dict = {"name": "prash", "age": 22, "phone": 7506392852}
+# # print(dict)
+# # print("type of  dict:", type(dict))
+# # print(dict["name"])
+# # dict["address"] ="agra"
+# # print(dict)
+# # dict["name"]= "prashansa" 
+# # dict.update({"age":26})
+# # del dict["phone"]
+# # print(dict)
+# # print(dict.keys())
+# # print(dict.values())
+# # print(dict.items())
+
+# # print("name" in dict)
+
+# # dict1 ={}
+# # print("type of dict1:", type(dict1))
+# # print (dict1)
+
+# # set
+
+# # s= {1,3,3,6,4,6,5,3,7,5,3,8,6}
+# # print(s)
+# # print(type(s))
+# # s.add(34)
+# # print(s)
+
+# # s.discard(34)
+# # print(s)
+
+# # mylist=[1,3,5,3,5,3,2]
+# # print(type(mylist))
+# # print(mylist)
+# # myset = set(mylist)
+# # print(myset)
+# # print(type(myset))
+
+# # copy and deep copy
+
+# # import copy
+
+# # list1=[1,2,3,4,[6,7,8]]
+# # list2=copy.copy(list1)
+
+# # list2[4][1]= 0
+# # print(list2)
+# # print(list1)
+
+# # list3=copy.deepcopy(list1)
+
+# # list3[4][2]= 10
+# # print(list3)
+# # print(list1)
+
+
+
+# # l1= [1,2,3,4,"abc","hjb","chj","dol"]
+# # for i in range(0,len(l1)):
+# #     t=type(l1[i])
+# #     if(t== "<class 'str'>"):
+# #         l1.remove
+    
+#     #    python operators
+
+# x=30
+# y=3
+
+# # arithmetic operators
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+# print(x**y)
+
+# # comparison operator
+
+# print(x==y)
+# print(x!=y)
+# print(x<y)
+# print(x>y)
+# print(x<=y)
+# print(x<=y)
+
+# # logical operators
+
+# a=True
+# b=False
+
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+# # assignment operators
+
+# x=50
+# x+=2
+# print (x)
+
+# x-=2
+# print(x)
+
+# x*=3
+# print(x)
+
+# x/=3
+# print(x)
+
+# x**=3
+# print(x)
+
+# x//=3
+# print(x)
+
+# x%=2
+# print(x)
+
+# # bitwise operators
+
+# x=2
+# y=3
+
+# print(x&y)
+# print(x | y)
+# print(x^y)
+# print(~x)
+# print(x<<1)
+# print(y>>1)
+
+# # identify operator
+
+# x=[1,2,3]
+# a=x
+# y=[1,2,3]
+# z=[1,4,3]
+
+# print(x is y)
+# print(x is z)
+# print(x is a)
+# print(a is y)
+# print(y is z)
+
+# # membership operator
+
+# l=[1,2,3,4,5,6,7,0]
+
+# print(2 in l)
+# print(8 in l)
+
+print()
